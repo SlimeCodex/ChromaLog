3f952a5af2af0a98b49e6ee82b75b5540549ca48
SlimeCodex <ale.nicolini.p@gmail.com>
2023-10-16 09:09:21 -0300

    fix license

--- src/ChromaLog.cpp
+++ src/ChromaLog.cpp
@@ -1,19 +1,19 @@
 /*
-* This file is part of SmartSyncEvent Library.
+* This file is part of ChromaLog Library.
 * Copyright (C) 2023 Alejandro Nicolini
 * 
-* SmartSyncEvent is free software: you can redistribute it and/or modify
+* ChromaLog is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
 * the Free Software Foundation, either version 3 of the License, or
 * (at your option) any later version.
 * 
-* SmartSyncEvent is distributed in the hope that it will be useful,
+* ChromaLog is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 * GNU General Public License for more details.
 * 
 * You should have received a copy of the GNU General Public License
-* along with SmartSyncEvent. If not, see <https://www.gnu.org/licenses/>.
+* along with ChromaLog. If not, see <https://www.gnu.org/licenses/>.
 */
 
 #include <ChromaLog.h>
