ea88db6c6c5d92e482718401d5c9f06287cac327
SlimeCodex <ale.nicolini.p@gmail.com>
2023-10-16 02:35:46 -0300

    code formatting

--- src/ChromaLog.cpp
+++ src/ChromaLog.cpp
@@ -18,40 +18,24 @@
 
 #include <ChromaLog.h>
 
+SemaphoreHandle_t ChromaLog::log_mutex = xSemaphoreCreateMutex();
+
 std::map<std::string, std::string> ChromaLog::colorMap = {
-	{"black", "\033[30m"},
-	{"red", "\033[31m"},
-	{"green", "\033[32m"},
-	{"yellow", "\033[33m"},
-	{"blue", "\033[34m"},
-	{"magenta", "\033[35m"},
-	{"cyan", "\033[36m"},
-	{"white", "\033[37m"},
+	{"black", "\033[90m"},
+	{"red", "\033[91m"},
+	{"green", "\033[92m"},
+	{"yellow", "\033[93m"},
+	{"blue", "\033[94m"},
+	{"magenta", "\033[95m"},
+	{"cyan", "\033[96m"},
+	{"white", "\033[97m"},
 
-	{"b_black", "\033[90m"},
-	{"b_red", "\033[91m"},
-	{"b_green", "\033[92m"},
-	{"b_yellow", "\033[93m"},
-	{"b_blue", "\033[94m"},
-	{"b_magenta", "\033[95m"},
-	{"b_cyan", "\033[96m"},
-	{"b_white", "\033[97m"},
-
-	{"bg_black", "\033[40m"},
-	{"bg_red", "\033[41m"},
-	{"bg_green", "\033[42m"},
-	{"bg_yellow", "\033[43m"},
-	{"bg_blue", "\033[44m"},
-	{"bg_magenta", "\033[45m"},
-	{"bg_cyan", "\033[46m"},
-	{"bg_white", "\033[47m"},
-
-	{"bg_b_black", "\033[100m"},
-	{"bg_b_red", "\033[101m"},
-	{"bg_b_green", "\033[102m"},
-	{"bg_b_yellow", "\033[103m"},
-	{"bg_b_blue", "\033[104m"},
-	{"bg_b_magenta", "\033[105m"},
-	{"bg_b_cyan", "\033[106m"},
-	{"bg_b_white", "\033[107m"}
+	{"bg_black", "\033[100m"},
+	{"bg_red", "\033[101m"},
+	{"bg_green", "\033[102m"},
+	{"bg_yellow", "\033[103m"},
+	{"bg_blue", "\033[104m"},
+	{"bg_magenta", "\033[105m"},
+	{"bg_cyan", "\033[106m"},
+	{"bg_white", "\033[107m"}
 };
--- src/ChromaLog.h
+++ src/ChromaLog.h
@@ -19,53 +19,127 @@
 #ifndef ___CHROMA_LOG_H___
 #define ___CHROMA_LOG_H___
 
-#include <cstdarg>
+#include "freertos/FreeRTOS.h"
+#include "freertos/semphr.h"
+#include "freertos/task.h"
+
 #include <string>
-#include <cstdio>
-#include <regex>
+#include <vector>
 #include <map>
 
+#include <Arduino.h>
+#define CHROMA_LOG_CHANNEL	Serial
+
 // LOG Helpers
-#define ANSI_RESET	"\033[0m"	// Reset color
-#define ANSI_CYAN	"\033[36m"	// For timestamp
+#define ANSI_RESET		"\033[0m"
+#define ANSI_BLACK		"\033[90m"
+#define ANSI_RED		"\033[91m"
+#define ANSI_GREEN		"\033[92m"
+#define ANSI_YELLOW		"\033[93m"
+#define ANSI_BLUE		"\033[94m"
+#define ANSI_MAGENTA	"\033[95m"
+#define ANSI_CYAN		"\033[96m"
+#define ANSI_WHITE		"\033[97m"
+#define ANSI_BG_BLACK	"\033[100m"
+#define ANSI_BG_RED		"\033[101m"
+#define ANSI_BG_GREEN	"\033[102m"
+#define ANSI_BG_YELLOW	"\033[103m"
+#define ANSI_BG_BLUE	"\033[104m"
+#define ANSI_BG_MAGENTA	"\033[105m"
+#define ANSI_BG_CYAN	"\033[106m"
+#define ANSI_BG_WHITE	"\033[107m"
+
+#define CHROMA_LOG_TIMESTAMP_COLOR	ANSI_BG_BLACK
+#define CHROMA_LOG_FILE_LINE_COLOR	ANSI_BG_BLACK
+#define CHROMA_LOG_FUNC_COLOR		ANSI_BG_BLACK
 
 class ChromaLog {
 private:
+	static SemaphoreHandle_t log_mutex;
 	static std::map<std::string, std::string> colorMap;
 
 	static std::string process(const std::string& message) {
-#ifndef CHROMA_LOG_COLOR_ENABLED
-		std::regex pattern(R"(~(\w+)\(([^)]+)\)~)");
-		return std::regex_replace(message, pattern, "$2");  // Strip out color tags, keep format specifiers
-#else
-		std::regex pattern(R"(~(\w+)\(([^)]+)\)~)");
-		std::smatch matches;
+#ifndef CHROMA_LOG_COLOR
+		// Find all patterns and simply remove them
 		std::string processed = message;
-
-		while (std::regex_search(processed, matches, pattern)) {
-			std::string replacement;
-			if (colorMap.find(matches[1]) != colorMap.end()) {
-				replacement = colorMap[matches[1].str()] + matches[2].str() + ANSI_RESET;
-			} else {
-				replacement = matches[2];  // if color not recognized, just use the format specifier
+		size_t start = 0;
+		while ((start = processed.find('~', start)) != std::string::npos) {
+			size_t openPar = processed.find('(', start);
+			size_t closePar = processed.find(')', openPar);
+			if (openPar != std::string::npos && closePar != std::string::npos) {
+				processed = processed.substr(0, start) + processed.substr(openPar + 1, closePar - openPar - 1) + processed.substr(closePar + 2);
 			}
-			processed = std::regex_replace(processed, pattern, replacement, std::regex_constants::format_first_only);
+			start = start + 1; // move past the current '~'
+		}
+		return processed;
+#else
+		std::string processed = message;
+		size_t start = 0;
+		while ((start = processed.find('~', start)) != std::string::npos) {
+			size_t openPar = processed.find('(', start);
+			size_t closePar = processed.find(')', openPar);
+			if (openPar != std::string::npos && closePar != std::string::npos) {
+				std::string color = processed.substr(start + 1, openPar - start - 1);
+				std::string content = processed.substr(openPar + 1, closePar - openPar - 1);
+
+				std::string replacement;
+				if (colorMap.find(color) != colorMap.end()) {
+					replacement = colorMap[color] + content + ANSI_RESET;
+				} else {
+					replacement = content;  // if color not recognized, just use the format specifier
+				}
+
+				processed = processed.substr(0, start) + replacement + processed.substr(closePar + 2);
+				start = start + replacement.length();
+			} else {
+				start = start + 1; // move past the current '~'
+			}
 		}
 		return processed;
 #endif
 	}
-
 public:
 	static void log(const char* file, int line, const char* func, const char* format, ...) {
-		std::string processedFormat = process(std::string(format));
+		if (xSemaphoreTake(log_mutex, portMAX_DELAY) == pdTRUE) {
 
-#ifdef CHROMA_LOG_OUTPUT_ADVANCED
-		printf("[%s:%d] %s(): ", file, line, func);
-#endif
-		va_list args;
-		va_start(args, format);
-		vprintf(processedFormat.c_str(), args);
-		va_end(args);
+			// Timestamp injection using FreeRTOS's xTaskGetTickCount
+			#ifdef CHROMA_LOG_TIMESTAMP
+			CHROMA_LOG_CHANNEL.printf(CHROMA_LOG_TIMESTAMP_COLOR " %" PRIu32 " " ANSI_RESET, xTaskGetTickCount());
+			#endif
+
+			// File and Line injection
+			#ifdef CHROMA_LOG_FILE_LINE
+			CHROMA_LOG_CHANNEL.printf(" ");
+			CHROMA_LOG_CHANNEL.printf(CHROMA_LOG_FILE_LINE_COLOR " %s:%d " ANSI_RESET, file, line);
+			#endif
+
+			// Function injection
+			#ifdef CHROMA_LOG_FUNC
+			CHROMA_LOG_CHANNEL.printf(" ");
+			CHROMA_LOG_CHANNEL.printf(CHROMA_LOG_FUNC_COLOR " %s " ANSI_RESET, func);
+			#endif
+
+			CHROMA_LOG_CHANNEL.printf(" ");
+
+			va_list args;
+			va_start(args, format);
+
+			int requiredSize = std::vsnprintf(nullptr, 0, format, args);
+			va_end(args);
+
+			if (requiredSize <= 0) return;
+
+			std::vector<char> buffer(requiredSize + 1);
+			va_start(args, format);
+			std::vsnprintf(buffer.data(), buffer.size(), format, args);
+			va_end(args);
+
+			std::string formattedMessage = buffer.data();
+			std::string processed = process(formattedMessage);
+
+			CHROMA_LOG_CHANNEL.printf("%s", processed.c_str());
+			xSemaphoreGive(log_mutex);
+		}
 	}
 };
 
